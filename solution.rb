c70aaa98e9ef43562f9d9e6e36a524053c8b6908
Paula <43395223+PaulaE89@users.noreply.github.com>
2019-04-25 22:23:51 -0500

    saludame1

--- solution.rb
+++ solution.rb
@@ -1,9 +1,10 @@
 require  'sinatra'
 get '/' do
 
-  if  params[:nombre]
-    "<h1>Hola #{params[:nombre]}!</h1>"
-  else
+  unless params[:nombre]|| params[:nombre]=="" 
     "<h1>Hola desconocido!<h1>"
+  
+    else
+      "<h1>Hola #{params[:nombre]}!</h1>"
+    end
   end
-end
